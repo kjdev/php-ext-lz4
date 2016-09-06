35f942c0e26f12999332d5043777ed5f0264a775
kj <kamijo@ninja.co.jp>
2016-09-07 08:35:28 +0900

    Updated version to 0.2.5

--- php_lz4.h
+++ php_lz4.h
@@ -24,7 +24,7 @@
 #ifndef PHP_LZ4_H
 #define PHP_LZ4_H
 
-#define LZ4_EXT_VERSION "0.2.4"
+#define LZ4_EXT_VERSION "0.2.5"
 
 extern zend_module_entry lz4_module_entry;
 #define phpext_lz4_ptr &lz4_module_entry
