4641acd452a7ea90de1347c3a2f0397357c155b8
kjdev <kjclev@gmail.com>
2022-05-27 10:13:06 +0900

    add: arginfo stubs

---
+++ lz4.stub.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace {
+
+  function lz4_compress(string $data, int $level = 0, string $extra = NULL): string|false {}
+
+
+  function lz4_uncompress(string $data, int $maxsize = -1, int $offset = -1): string|false {}
+
+}
