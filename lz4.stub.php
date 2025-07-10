1d529b9d7ce0d895694ae6f62772e5221b8cc93f
kjdev <kjclev@gmail.com>
2025-07-11 08:03:27 +0900

    chore: add constants to lz4.stub.php

--- lz4.stub.php
+++ lz4.stub.php
@@ -2,6 +2,31 @@
 
 namespace {
 
+  /**
+   * @var int
+   * @cvalue LZ4_CLEVEL_MIN
+   */
+  const LZ4_CLEVEL_MIN = UNKNOWN;
+
+  /**
+   * @var int
+   * @cvalue LZ4_CLEVEL_MAX
+   */
+  const LZ4_CLEVEL_MAX = UNKNOWN;
+
+  /**
+   * @var string
+   * @cvalue LZ4_VERSION_TEXT
+   */
+  const LZ4_VERSION_TEXT = UNKNOWN;
+
+  /**
+   * @var int
+   * @cvalue LZ4_VERSION_NUMBER
+   */
+  const LZ4_VERSION_NUMBER = UNKNOWN;
+
+
   function lz4_compress(string $data, int $level = 0, string $extra = NULL): string|false {}
 
 
