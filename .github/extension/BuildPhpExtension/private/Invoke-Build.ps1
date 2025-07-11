ae8916d9f61999bda752980bf36fe023515647f3
kjdev <kjclev@gmail.com>
2025-07-11 10:05:30 +0900

    fixup! wip: ci

--- .github/extension/BuildPhpExtension/private/Invoke-Build.ps1
+++ .github/extension/BuildPhpExtension/private/Invoke-Build.ps1
@@ -30,6 +30,11 @@ Function Invoke-Build {
             if($env:ARTIFACT_NAMING_SCHEME -eq 'pecl') {
                 $ref = $Config.ref.ToLower()
             }
+            echo ">>> builder: $builder"
+            echo ">>> -c: $Config.vs_version"
+            echo ">>> -a: $Config.Arch"
+            echo ">>> -s: $Config.vs_toolset"
+            echo ">>> -t: $task"
             $suffix = "php_" + (@(
                 $Config.name,
                 $ref,
@@ -38,10 +43,7 @@ Function Invoke-Build {
                 $Config.vs_version,
                 $Config.arch
             ) -join "-")
-            # & $builder -c $Config.vs_version -a $Config.Arch -s $Config.vs_toolset -t $task | Tee-Object -FilePath "build-$suffix.txt"
-            echo "$builder -c $Config.vs_version -a $Config.Arch -s $Config.vs_toolset -t $task"
-            echo "build-$suffix.txt"
-            $builder -c $Config.vs_version -a $Config.Arch -s $Config.vs_toolset -t $task
+            & $builder -c $Config.vs_version -a $Config.Arch -s $Config.vs_toolset -t $task | Tee-Object -FilePath "build-$suffix.txt"
             Set-GAGroup end
             if(-not(Test-Path "$((Get-Location).Path)\$($Config.build_directory)\php_$($Config.name).dll")) {
                 throw "Failed to build the extension"
