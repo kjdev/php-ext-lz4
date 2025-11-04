e0943f89bf8daddf25047b9dff2a4f21bbfb2bbe
Andreas Wahlen <andreas.wahlen@nerou.de>
2025-11-04 22:11:19 +0100

    Add docblock to stubs

--- lz4.stub.php
+++ lz4.stub.php
@@ -32,6 +32,10 @@ namespace {
 
   function lz4_uncompress(string $data, int $maxsize = -1, int $offset = -1): string|false {}
 
+  /**
+   * @param int $max_block_size 4: 64KB, 5: 256KB, 6: 1MB, 7: 4MB, all other values: 64KB
+   * @param int $checksums 0: none, 1: frame content, 2: each block, 3: frame content + each block
+   */
   function lz4_compress_frame(string $data, int $level = 0, int $max_block_size = 0, int $checksums = 0): string|false {}
 
 
