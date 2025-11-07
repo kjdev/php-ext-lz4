bf27b2c13922b6fbeb2eac5251be4e8a95d01e36
kjdev <kjclev@gmail.com>
2025-11-07 11:17:19 +0900

    upgrade: version to 0.6.0

--- php_lz4.h
+++ php_lz4.h
@@ -24,7 +24,7 @@
 #ifndef PHP_LZ4_H
 #define PHP_LZ4_H
 
-#define LZ4_EXT_VERSION "0.5.0"
+#define LZ4_EXT_VERSION "0.6.0"
 
 extern zend_module_entry lz4_module_entry;
 #define phpext_lz4_ptr &lz4_module_entry
